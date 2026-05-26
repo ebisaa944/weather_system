d2405b12fb6d834c5c975d16f385e5a7949cd4a4
ebisaa944@ <ebisaa944@gmail.com>
2026-05-26 16:56:42 +0300

    Fix static files deployment (Whitenoise + collectstatic)

--- weather_system/settings.py
+++ weather_system/settings.py
@@ -272,9 +272,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
